b8949ba4577c870ac36e214ca4bd5f4bba47722e
Vasile Botoroga <vbotoroga@gmail.com>
2017-09-20 15:10:11 +0200

    fix Redefinition of RCTMethodInfo in rn 0.48+

--- ios/ReactNativeNfcIos.h
+++ ios/ReactNativeNfcIos.h
@@ -1,10 +1,10 @@
 #import <Foundation/Foundation.h>
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import "RCTEventEmitter.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+    #import <React/RCTBridgeModule.h>
+    #import <React/RCTEventEmitter.h>
 #else
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
+    #import "RCTBridgeModule.h"
+    #import "RCTEventEmitter.h"
 #endif
 
 @interface ReactNativeNfcIos : RCTEventEmitter <RCTBridgeModule>
